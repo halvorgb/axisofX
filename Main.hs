44246d69dca2d71ea0bd9d663a3b05a3763dbb8a
Halvor G B <halvorgb@gmail.com>
2013-06-16 15:38:46 +0200

    Tutorial 1 complete

--- Main.hs
+++ Main.hs
@@ -12,7 +12,52 @@ data Input = Up | Down | Left | Right | Exit
            deriving (Eq)
 
 main = do
+  hSetEcho stdin False
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
+  hideCursor
+  setTitle "Axis of X"
+  gameLoop $ World 0 -- initialized with position 0?
+
+gameLoop world@(World heroX) = do
+  drawHero heroX
+  input <- getInput
+  case input of
+    Exit -> handleExit
+    _ -> handleDir world input
+
+drawHero heroX = do
+  clearScreen
+  setCursorPosition 0 heroX
   setSGR [ SetConsoleIntensity BoldIntensity,
            SetColor Foreground Vivid Blue ]
   putStrLn "@"
+
+
+getInput = do
+  char <- getChar
+  case char of
+    'q' -> return Exit
+    'w' -> return Up
+    'a' -> return Left
+    's' -> return Down
+    'd' -> return Right
+    _   -> getInput -- recurse if input not recognized
     
+handleDir w@(World heroX) input =
+  gameLoop $ w { wHero = boundsCheck newX }
+    where
+      newX = case input of 
+        Up -> heroX
+        Left -> heroX - 1
+        Down -> heroX
+        Right -> heroX + 1
+      boundsCheck = (\x -> (max 0 (min x 100)))
+
+handleExit = do
+  clearScreen
+  setCursorPosition 0 0
+  showCursor
+  setSGR [ Reset ]
+  putStrLn "Thank you for playing Axis of X!"
+  
