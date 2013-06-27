b6a73d5b339b4a18d374bd0d244a883b49d12344
Halvor G B <halvorgb@gmail.com>
2013-06-27 04:42:53 +0200

    starting random world generator

--- WorldBuilder.hs
+++ WorldBuilder.hs
@@ -3,6 +3,24 @@ module LevelGenerator where
 import Level
 import Types
 
+import System.Random
+import System.IO.Unsafe
+
+lengthBounds = (100, 300) -- minimum and maximum length of a level
+viewRange = (20, 40) -- minimum and maximum viewRange (+10 for backwards viewing)
+
+-- returnWorld :: I
+returnWorld = do
+  gen <- getStdGen
+  let world = generateWorld gen
+  return world
+
+
+--generateWorld :: StdGen -> IO In
+generateWorld gen0 = (num1, num2)
+  where
+    (num1,gen1) = next gen0
+    (num2,gen2) = next gen1
 -- generateLevel takes a seed and  generates the whole level.
 generateLevel = undefined
 
